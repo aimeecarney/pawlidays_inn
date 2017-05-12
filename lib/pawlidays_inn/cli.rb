d0399c2710c71d87cdf6cee0ece680a55091bded
Aimee Carney <acarney29@gmail.com>
2017-05-12 01:34:38 +0000

    added more structure to cli and adjusted URL in scraper

--- lib/pawlidays_inn/cli.rb
+++ lib/pawlidays_inn/cli.rb
@@ -9,18 +9,40 @@ class PawlidaysInn::CLI
   end
 
   def start
+
     puts ""
     puts "Please enter the city you would like to visit with Perro:"
-    location_city = gets.chomp
+    city = gets.strip
     puts "Please enter the state abbreviation the city is in:"
-    location_state = gets.chomp
-    puts "Please enter when you need to check in YYYY-MM-DD:"
-    date_checkin = gets.chomp
-    puts "Please enter when you need to check out YYYY-MM-DD:"
-    date_checkout = gets.chomp
+    state = gets.strip
     binding.pry
+
+    PawlidaysInn::Scraper.new.get_page(city, state)
+
+    print_listings #print all listings with scrape/listing class
+
+    while input != "exit"
+      puts ""
+      puts "Which listing would you like more information on?"
+      puts ""
+      puts "Enter list to see the listings again."
+      puts "Enter exit to end the program."
+      puts ""
+      input = gets.strip
+      if input == "list"
+        print_listings
+      elsif input.to_1 > 0
+        #print listing chosen from number on list
+      end
+    end
+
+
   end
 
+  def print_listing
 
+  end
+
+  # AIR BNB URL https://www.airbnb.com/s/#{city}-#{state}/homes?adults=2&allow_override%5B%5D=&amenity=pet-friendly&checkin=#{checkin}&checkout=#{checkout}&gclid=CjwKEAjw9MrIBRCr2LPek5-h8U0SJAD3jfhtl9hkPiVSqmTAhWk03OtJQFsQedru2tQUUNu2WgYTgBoCqBzw_wcB&guests=2&hosting_amenities%5B%5D=12&in_vr=1&room_types%5B%5D=Entire%20home%2Fapt&s_tag=pkYHg0Tf
 
 end
--- lib/pawlidays_inn/scraper.rb
+++ lib/pawlidays_inn/scraper.rb
@@ -1,4 +1,21 @@
+require 'pry'
+
 class PawlidaysInn::Scraper
+
+  def get_page(city, state)
+    Nokogiri::HTML(open("https://www.airbnb.com/s/#{URI.encode(city)}—-#{URI.encode(state)}-—United-States/homes?allow_override%5B%5D=&hosting_amenities%5B%5D=12&s_tag=EVviYajS"))
+    binding.pry
+  end
+
+#  def scrape_page
+#    self.get_page.css("")
+#  end
+#
+#  def make_listings
+#    scrape_page_index.each do |r|
+#      WorldsBestRestaurants::Restaurant.new_from_index_page(r)
+#    end
+#  end
 
 
 end
